8178ecbbf4e2b75e432661d359527c4c8e65abd5
Alan451 <alantthomas451@gmail.com>
2020-08-01 12:25:37 +0530

    basic structure done

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'drawer.dart';
+
+class HomeScreen extends StatefulWidget {
+  static String id = 'home_screen';
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+  List<Widget> widgetOptions = [
+    //TODO:Feed widget
+    Container(
+      color: Colors.red,
+    ),
+    //TODO:Mess Menu viewer widget
+    Container(
+      color: Colors.black54,
+    ),
+    //TODO:Timings Viewer Widget
+    Container(
+      color: Colors.white,
+    ),
+  ];
+  List<Text> headingOptions = [
+    Text('home.'),
+    Text('mess menu.'),
+    Text('timings.'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: headingOptions[selectedIndex],
+          backgroundColor: Color(0xff3F80FA),
+        ),
+        drawer: drawerWidget,
+        body: widgetOptions[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.restaurant_menu),
+              title: Text('Mess Menu'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              title: Text('Timings'),
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: (int i) {
+            setState(() {
+              selectedIndex = i;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
