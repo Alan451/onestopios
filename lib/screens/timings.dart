2a3ff3f625533a5f64de416f58189efe18f2c0e9
anjalisoni3655 <anjalisony32@gmail.com>
2020-08-03 00:12:55 +0530

    buttons added

--- lib/screens/timings.dart
+++ lib/screens/timings.dart
@@ -2,34 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:onestop_ios/screens/InternalBus.dart';
 import 'IitgBus.dart';
 import 'Ferry.dart';
+import 'package:onestop_ios/components/CustumButton.dart';
 class Timing extends StatelessWidget {
   static const id = 'timing';
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-             onPressed: () {
-              Navigator.pushNamed(context, IitgBus.id);
-            },
-            child: Text('IITG Bus'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, InternalBus.id);
-            },
-            child: Text('Internal Bus'),
-          ),
-          RaisedButton(
-             onPressed: () {
-              Navigator.pushNamed(context, Ferry.id);
-            },
-            child: Text('Ferry'),
-          ),
-          // FlatButton.icon(onPressed: null, icon: null, label: null),
-        ],
+      child: Container(
+         margin: EdgeInsets.symmetric(
+                                horizontal:10.0,
+                                vertical: 10.0),
+        child: Column(
+          children: <Widget>[
+             SizedBox(
+height:10.0,
+            ),
+            CustomButton(
+              title:'IITG Bus',
+              image: "assets/6.png",
+               onPressed: () {
+                Navigator.pushNamed(context, IitgBus.id);
+              },
+             
+            ),
+            SizedBox(
+height:10.0,
+            ),
+            CustomButton(
+              title:'Internal Bus',
+              image: "assets/6.png",
+              onPressed: () {
+                
+                Navigator.pushNamed(context, InternalBus.id);
+              },
+             
+            ),
+             SizedBox(
+height:10.0,
+            ),
+            CustomButton(
+              title: 'Ferry',
+              image: "assets/6.png",
+               onPressed: () {
+                Navigator.pushNamed(context, Ferry.id);
+              },
+             
+            ),
+            // FlatButton.icon(onPressed: null, icon: null, label: null),
+          ],
+        ),
       ),
     );
   }
